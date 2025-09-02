53fe3caee52dab4e74e1ae9a65bfc40083fd096b
Tuzink <mirandaartur1230@gmail.com>
2025-09-01 22:31:46 -0300

    Criado o projeto TipTime

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
+        {
+           PorcentagemSlider.Value = 15;
+        }
+        
+
+        private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+        }
+
+        private void RoundD_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void RoundU_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            float 
+
+
+        }
+    }
+
+}
