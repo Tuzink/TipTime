01344869020f3eaf71516930111eecad0dd8e74c
Tuzink <mirandaartur1230@gmail.com>
2025-09-08 22:31:46 -0300

    Adcionados métodos slider e cálculo da gorjeta

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,11 +2,14 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        //int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            ValorTotalEntry.Text = "0";
+            PorcentagemSlider.Value = 15;
+
         }
 
         private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
@@ -32,8 +35,28 @@
 
         private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            float 
+            try {
 
+                double porcentagem = PorcentagemSlider.Value;
+                PorcentagemLabel.Text = $"{porcentagem}%";
+                //double gorjeta;
+                double valortotal = double.Parse(ValorTotalEntry.Text);
+                //double valortotal = Convert.toDouble(ValorTotalEntry.Text); outro jeito de definir a variável valortotal :)
+                //Convert é uma classe
+                double valorfinal;
+                double gorjeta = valortotal * (porcentagem / 100);
+                //ValorGorjeta.Text = $"R$ {gorjeta}";
+                ValorGorjeta.Text = gorjeta.ToString("C");
+                //TotalFinal.Text = $"R$ {valortotal}";
+                TotalFinal.Text = valortotal.ToString("C");
+            }
+            catch (Exception ex)
+            {
+                //toda vez que uso o comandoi Console.WriteLine a saída vai para a 
+                //console na aba output
+                Console.WriteLine(ex.ToString());
+                DisplayAlert("Erro", "Digite um valor numérico", "OK");
+            }
 
         }
     }
